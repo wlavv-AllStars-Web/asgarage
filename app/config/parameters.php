3d37fa9d3cb6795e5f95810f5300880c3e5b4ef8
PauloDavidWebSolutions <141250325+PauloDavidWebSolutions@users.noreply.github.com>
2024-03-07 11:40:56 +0000

    parametes prod

--- app/config/parameters.php
+++ app/config/parameters.php
@@ -4,7 +4,7 @@
     'database_host' => 'localhost',
     'database_port' => '',
     'database_name' => 'asw01032024_asgarage',
-    'database_user' => 'asw01032024_asgarage',
+    'database_user' => 'asw01032024_asw01032024',
     'database_password' => 'IiSEtB|£4F}35I#D',
     'database_prefix' => 'ps_',
     'database_engine' => 'InnoDB',
